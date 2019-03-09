307cdd5b99c22d0bbd0a063a04125ee0da7a6ba3
Qiuqiong Zhou <qiuqiongzhou@admins-MacBook-Pro.local>
2019-03-09 14:44:23 +0100

    Added ExerciseFileForVScodeTraining

---
+++ ExerciseFilesVScodeWebDev/Exercise Files/_scripts/script.js
@@ -0,0 +1,19 @@
+function f() {
+    var a = []
+}
+//Essential extentions jQuery Code Snippets
+$.ajax({
+    type: "method",
+    url: "url",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        
+    }
+});
+//Working with snippets
+function QQsFirstSnippets(p1, p2, p3) {
+    return;
+
+    var elem = document.querySelector('#QQid');
+}
